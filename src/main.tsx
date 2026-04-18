d83aba3ca94384b84f12f60e748e322959b67f56
relsam-atinersier <github-personal.abacus946@passmail.net>
2026-04-18 16:35:52 -0500

    define frontend skeleton

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot, type Root } from 'react-dom/client';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root: Root = createRoot(rootElement);
+  root.render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
